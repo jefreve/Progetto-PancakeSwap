f19d1398edc0f91725e5843720890f84f75ed4a7
Jef <jefersonrevellobiz@gmail.com>
2023-08-10 10:38:32 +0200

    feature: component/navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-purple": "#9A6AFF",
+        "buttons-cyan": "#1EC7D3",
+        "buttons-dark-text": "#191326",
+        "light-violet": "#B8ADD2",
+        "navbar-item-hover": "#353547",
+        "dropdown-border": "#383241",
+        "green-dot": "#31d0aa",
+        "secondary-red": "#ED4B9E",
+        "navbar-icon-hover": "#857E98",
+      },
+    },
   },
   plugins: [],
 };
