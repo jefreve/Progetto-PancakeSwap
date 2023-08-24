9381419bc12c6d16984a6448e2d6a87b3d922aa6
Jef <jefersonrevellobiz@gmail.com>
2023-08-24 15:02:41 +0200

    feat: component/earn-passive-income

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ["Kanit, sans-serif"],
+    },
+    extend: {
+      colors: {
+        "main-purple": "#9A6AFF",
+        "buttons-cyan": "#1EC7D3",
+        "buttons-dark-text": "#191326",
+        "light-violet": "#B8ADD2",
+        "navbar-item-hover": "#353547",
+        "dropdown-border": "#383241",
+        "green-dot": "#31d0aa",
+        "secondary-red": "#ED4B9E",
+        "navbar-icon-hover": "#857E98",
+      },
+    },
   },
   plugins: [],
 };
