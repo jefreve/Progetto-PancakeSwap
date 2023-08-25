b24e7be700f45ea58e488eb1782332a4a2d6909b
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-08-25 15:59:38 +0200

    Bunny section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        bounce: "duration-100 ease-in-out delay-0 infinite",
+      },
+      keyframes: {
+        fade: {
+          "0%,100%": {
+            opacity: "1",
+          },
+          "50%": {
+            opacity: "0.2",
+          },
+        },
+        bounce: {
+          "0%, 100%": { transform: "translate(0px)" },
+          "50%": { transform: "translate(-7px, -7px)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
