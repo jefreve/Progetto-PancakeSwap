4042f182214a7c280a4bd54d2ca1b1e7b9bb8941
Lorenzo Cazzato <lorenzocazzato98@icloud.com>
2023-08-28 20:02:24 +0200

    fix taillwindconfi tradeanything

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,45 +32,41 @@ module.exports = {
           "stonksFloating 3s ease-in-out 0.66s infinite normal none running",
         folderFloating:
           "stonksFloating 3s ease-in-out 0.33s infinite normal none running",
-      },
-      keyframes: {
-        pieFloating: {
-          "0%, 100%": { transform: "translate(0px, 0px)" },
-          "50%": { transform: "translate(3px, 15px)" },
-        },
-        stonksFloating: {
-          "0%, 100%": { transform: "translate(0px, 0px)" },
-          "50%": { transform: "translate(5px, 10px)" },
-        },
-        folderFloating: {
-          "0%, 100%": { transform: "translate(0px, 0px)" },
-          "50%": { transform: "translate(5px, 6px)" },
-        },
-      },
-      boxShadow: {
-        button: "0px -1px 0px 0px inset",
-        selectNetwork: "0px -2px 0px 0px inset",
-
-        animation: {
-          bounce: "duration-100 ease-in-out delay-0 infinite",
-        },
-        keyframes: {
-          fade: {
-            "0%,100%": {
-              opacity: "1",
-            },
-            "50%": {
-              opacity: "0.2",
-            },
-          },
-          bounce: {
-            "0%, 100%": { transform: "translate(0px)" },
-            "50%": { transform: "translate(-7px, -7px)" },
-          },
-        },
+        bounce: "duration-100 ease-in-out delay-0 infinite",
       },
     },
-    plugins: [],
-    darkMode: "class",
+    boxShadow: {
+      button: "0px -1px 0px 0px inset",
+      selectNetwork: "0px -2px 0px 0px inset",
+    },
+
+    keyframes: {
+      fade: {
+        "0%,100%": {
+          opacity: "1",
+        },
+        "50%": {
+          opacity: "0.2",
+        },
+      },
+      bounce: {
+        "0%, 100%": { transform: "translate(0px)" },
+        "50%": { transform: "translate(-7px, -7px)" },
+      },
+      pieFloating: {
+        "0%, 100%": { transform: "translate(0px, 0px)" },
+        "50%": { transform: "translate(3px, 15px)" },
+      },
+      stonksFloating: {
+        "0%, 100%": { transform: "translate(0px, 0px)" },
+        "50%": { transform: "translate(5px, 10px)" },
+      },
+      folderFloating: {
+        "0%, 100%": { transform: "translate(0px, 0px)" },
+        "50%": { transform: "translate(5px, 6px)" },
+      },
+    },
   },
+  plugins: [],
+  darkMode: "class",
 };
